45b33d5cbeaec6c0d1dc869e3b4d07363fefbf8f
Daniyel-Tkachuk <danilaradyn7357899@gmail.com>
2025-05-06 22:36:47 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
 import './App.css'
+import {Todolist} from "./components/Todolist.tsx";
 
-function App() {
-  return (
-      <div className="app">
-        <div>
-          <h3>What to learn</h3>
-          <div>
-            <input/>
-            <button>+</button>
-          </div>
-          <ul>
-            <li>
-              <input type="checkbox" checked={true}/> <span>HTML&CSS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={true}/> <span>JS</span>
-            </li>
-            <li>
-              <input type="checkbox" checked={false}/> <span>React</span>
-            </li>
-          </ul>
-          <div>
-            <button>All</button>
-            <button>Active</button>
-            <button>Completed</button>
-          </div>
-        </div>
-      </div>
-  )
+export type TaskType = {
+  id: number
+  title: string
+  isDone: boolean
 }
 
-export default App
+export const App = () => {
+  const tasks1: TaskType[] = [
+    {id: 1, title: "task1", isDone: true},
+    {id: 2, title: "task2", isDone: false},
+    {id: 3, title: "task3", isDone: false},
+  ]
+
+  const tasks2: TaskType[] = [
+    {id: 4, title: "task4", isDone: true},
+    {id: 5, title: "task5", isDone: true},
+    {id: 6, title: "task6", isDone: false},
+  ]
+
+  const tasks3: TaskType[] = []
+
+  return (
+    <div className="app">
+      <Todolist title={"task-1"} tasks={tasks1}/>
+      <Todolist title={"task-2"} tasks={tasks2}/>
+      <Todolist title={"task-3"} tasks={tasks3}/>
+    </div>
+  )
+}
