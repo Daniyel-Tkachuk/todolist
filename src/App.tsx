96f70c97af985112fdc76ab2524de7f6d9ccdde4
Daniyel-Tkachuk <danilaradyn7357899@gmail.com>
2025-05-14 14:31:46 +0300

    refactor before lessons

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,19 @@ export type TaskType = {
 }
 
 export const App = () => {
-  const tasks1: TaskType[] = [
-    {id: 1, title: "task1", isDone: true},
-    {id: 2, title: "task2", isDone: false},
-    {id: 3, title: "task3", isDone: false},
+  const tasks: TaskType[] = [
+    {id: 1, title: "HTML&CSS", isDone: true},
+    {id: 2, title: "JS", isDone: false},
+    {id: 3, title: "ReactJS", isDone: false},
+    {id: 4, title: "Redux", isDone: false},
+    {id: 5, title: "TypeScript", isDone: false},
+    {id: 6, title: "RTK query", isDone: false},
   ]
 
 
   return (
     <div className="app">
-      <Todolist title={"task-1"} tasks={tasks1}/>
+      <Todolist title={"task-1"} tasks={tasks}/>
     </div>
   )
 }
