09aab366ea1742c632ba58d304cb43fb0da4741a
Daniyel-Tkachuk <danilaradyn7357899@gmail.com>
2025-09-13 22:54:59 +0300

    refactor

--- src/features/todolists/ui/Todolists/TodolistItem/FilterButtons/FilterButtons.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/FilterButtons/FilterButtons.tsx
@@ -1,11 +1,11 @@
 import { useAppDispatch } from "@/common/hooks"
 import { containerSx } from "@/common/styles"
-import { changeTodolistFilterAC, type FilterValues, type Todolist } from "@/features/todolists/model/todolistsSlice.ts"
+import { changeTodolistFilterAC, type FilterValues, type DomainTodolists } from "@/features/todolists/model/todolistsSlice.ts"
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
 
 type Props = {
-  todolist: Todolist
+  todolist: DomainTodolists
 }
 
 export const FilterButtons = ({ todolist }: Props) => {
